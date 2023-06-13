513e3fea3e22a1047064bbd173ff11a5cd180637
hyunseoKoo <koohs414@naver.com>
2023-06-14 01:11:12 +0900

    답 수정

--- 프로그래머스/1.js
+++ 프로그래머스/1.js
@@ -3,16 +3,16 @@
 numbers의 원소를 앞에서부터 하나씩 더하다가 그 합이 n보다 커지는 순간 이때까지 더했던 원소들의 합을 return 하는 solution 함수를 작성해 주세요.
  */
 
-let numbers = [34, 5, 71, 29, 100, 34];
-let n;
-let init = 0;
 function solution(numbers, n) {
-    numbers.forEach(element => {
-        init += element;
-    });
-        if(init < n) return;
-        return init;
+    let answer = 0;
+    for(let i=0; i<numbers.length; i++) {
+        if (answer < n) {
+            answer = answer + numbers[i];
+        }
+        else return answer - numbers[i-1];
+    }
+    return answer;
 };
 
 const result = solution([34, 5, 71, 29, 100, 34], 123);
-console.log(result);
+console.log(result); // 110
